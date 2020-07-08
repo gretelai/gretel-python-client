8a6656fce716d0931ba808cb506c098ff66b79c9
Drew Newberry <drew@gretel.ai>
2020-07-08 15:49:41 -0700

    use logger + more tests

--- src/gretel_client/pkg_installers.py
+++ src/gretel_client/pkg_installers.py
@@ -1,4 +1,4 @@
-import selectors
+import logging
 import sys
 import subprocess
 
@@ -6,34 +6,37 @@ import requests
 
 
 PKG_ENDPOINT = "https://{}/opt/pkg"
-
 TX_PKG = "gretel-transformers"
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(levelname)s %(filename)s: %(message)s")
+logger.setLevel(logging.INFO)
 
 
 class GretelInstallError(Exception):
     pass
 
 
-def read_pipe(section, cmd, p, verbose, collect=False):
+def read_pipe(cmd, p, verbose, log=logger.info, collect=False):
     out = [
-        f"\n{section}\n{''.join(['=' for _ in range(len(section)+1)])}",
         " ".join(cmd),
     ]
     if verbose and not collect:
         for line in out:
-            print(line)
+            log(line)
     for line in iter(p.readline, b""):
         if isinstance(line, bytes):
             line = line.decode("utf-8").strip()  # type: ignore
         if verbose and not collect:
-            print(line)
+            log(line)
         if collect:
             out.append(line)
 
-    if verbose and collect and len(out) > 2:
+    if verbose and collect and len(out) > 1:
         for line in out:
-            print(line)
-        print("")
+            log(line)
+        log("")
 
 
 def _install_pip_dependency(dep: str, verbose: bool = True):
@@ -47,11 +50,13 @@ def _install_pip_dependency(dep: str, verbose: bool = True):
         dep,
     ]
     if verbose:
-        print(f"running: {' '.join(cmd)}")
+        logger.info(f"running: {' '.join(cmd)}")
     results = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    read_pipe("pip: Install Output", cmd, results.stdout, verbose)
-    read_pipe("pip: Errors & Warnings", cmd, results.stderr, verbose=True, collect=True)
+    read_pipe(cmd, results.stdout, verbose)
+    read_pipe(
+        cmd, results.stderr, verbose=True, log=logger.error, collect=True,
+    )
 
 
 def _get_package_endpoint(package_identifier: str, api_key: str, host: str) -> str:
@@ -69,10 +74,10 @@ def _get_package_endpoint(package_identifier: str, api_key: str, host: str) -> s
     return source_pkg
 
 
-def install_transformers(api_key: str, host: str, verbose: bool = False):
-    print("Authenticating with package manager")
+def install_packages(api_key: str, host: str, verbose: bool = False):
+    logger.info("Authenticating with package manager")
     package_endpoint = _get_package_endpoint(TX_PKG, api_key, host)
 
-    print("Installing packages (this might take a while)")
+    logger.info("Installing packages (this might take a while)")
     _install_pip_dependency(package_endpoint, verbose)
-    print("Finished installing Gretel packages")
+    logger.info("Finished installing Gretel packages")
