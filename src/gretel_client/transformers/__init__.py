36a520e682327947286256a80a8e1e9ee0a91db9
John T. Myers <9696606+johntmyers@users.noreply.github.com>
2020-07-24 08:59:25 -0400

    Don't import FPE-enabled modules if deps are missing (#28)
    Co-authored-by: John Myers <john@gretel.ai>

--- src/gretel_client/transformers/__init__.py
+++ src/gretel_client/transformers/__init__.py
@@ -12,7 +12,6 @@ from gretel_client.transformers.transformers.bucket import (
 )
 from gretel_client.transformers.transformers.combine import CombineConfig
 from gretel_client.transformers.transformers.conditional import ConditionalConfig
-from gretel_client.transformers.transformers.date_shift import DateShiftConfig
 from gretel_client.transformers.transformers.drop import DropConfig
 from gretel_client.transformers.transformers.fake_constant import FakeConstantConfig
 from gretel_client.transformers.transformers.format import FormatConfig
@@ -25,9 +24,16 @@ from gretel_client.transformers.transformers.redact_with_label import (
 from gretel_client.transformers.transformers.redact_with_string import (
     RedactWithStringConfig,
 )
-from gretel_client.transformers.transformers.fpe_float import FpeFloatConfig
-from gretel_client.transformers.transformers.fpe_string import FpeStringConfig
 from gretel_client.transformers.transformers.secure_hash import SecureHashConfig
+
+# NOTE: the imports below require the "fpe" extras
+
+try:
+    from gretel_client.transformers.transformers.fpe_float import FpeFloatConfig
+    from gretel_client.transformers.transformers.fpe_string import FpeStringConfig
+    from gretel_client.transformers.transformers.date_shift import DateShiftConfig
+except ImportError:
+    pass
 
 
 __all__ = [
