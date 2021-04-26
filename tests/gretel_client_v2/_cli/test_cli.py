913150e97413c8b97fad7ff4399409cde2152fc7
Drew Newberry <drew@gretel.ai>
2021-04-26 09:10:08 -0700

    add cli smoke test
    GitOrigin-RevId: 69251c5e4ad5477ee4d804515b9c962cb166c162

--- tests/gretel_client_v2/_cli/test_cli.py
+++ tests/gretel_client_v2/_cli/test_cli.py
@@ -1,4 +1,16 @@
+import pytest
+
+from click.testing import CliRunner
+
+from gretel_client_v2._cli.cli import cli
 
 
-def test_cli():
-    assert True
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+def test_cli(runner):
+    call = runner.invoke(cli, ["--help"])
+    assert call.exit_code == 0
+    assert call.output.startswith("Usage")
