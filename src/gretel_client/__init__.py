aeb3dfe664d4a03529ad30165dca94a213eb5456
John T. Myers <9696606+johntmyers@users.noreply.github.com>
2022-07-06 15:16:51 -0400

    allow create_or_get _unique_project to be imported from top level pkg
    Co-authored-by: John Myers <john@gretel.ai>
    GitOrigin-RevId: 3013f4e033674bd73c22185c17a6d6556a4af5c7

--- src/gretel_client/__init__.py
+++ src/gretel_client/__init__.py
@@ -2,4 +2,8 @@
 from gretel_client.config import ClientConfig, configure_session
 from gretel_client.evaluation.quality_report import QualityReport
 from gretel_client.helpers import poll, submit_docker_local
-from gretel_client.projects.projects import create_project, get_project
+from gretel_client.projects.projects import (
+    create_or_get_unique_project,
+    create_project,
+    get_project,
+)
