24a243d50e1336362d22b73dc98c583a3dceb400
Johnny Greco <jgreco.ai@gmail.com>
2023-12-01 14:36:04 -0500

    better tuner importing experience
    GitOrigin-RevId: e7d278578e355df531687c1e07106b2d2c723e89

--- src/gretel_client/tuner/__init__.py
+++ src/gretel_client/tuner/__init__.py
@@ -1,2 +1,7 @@
-from gretel_client.tuner.metrics import BaseTunerMetric
+from gretel_client.tuner.metrics import (
+    BaseTunerMetric,
+    GretelMetricName,
+    GretelQualityScore,
+    MetricDirection,
+)
 from gretel_client.tuner.tuner import GretelTuner
