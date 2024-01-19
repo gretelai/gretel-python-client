6441bf7f3b2089d78fc3207a981da07389273c33
Matt Kornfield <kornfield@gretel.ai>
2024-01-19 14:02:46 -0800

    Add top level helpers for hybrid to gretel_client
    GitOrigin-RevId: 29b576f037f340790009e131073f280849d3e15b

--- src/gretel_client/__init__.py
+++ src/gretel_client/__init__.py
@@ -1,4 +1,9 @@
 # flake8: noqa
+import gretel_client._hybrid.aws as aws_hybrid
+import gretel_client._hybrid.azure as azure_hybrid
+import gretel_client._hybrid.gcp as gcp_hybrid
+
+from gretel_client._hybrid.config import configure_hybrid_session
 from gretel_client.config import ClientConfig, configure_session
 from gretel_client.evaluation.quality_report import QualityReport
 from gretel_client.gretel.interface import Gretel
