e1daa8351937cf781e38abbb2594737367d800bd
Nabin Mulepati <nabinchha@gmail.com>
2025-02-28 12:20:14 -0700

    [INTERNAL] PLAT-3237 - Config repair for AIDD chat
    Co-authored-by: Nabin Mulepati <nabinatgretel@mac.lan>
    GitOrigin-RevId: 905d8ea889819dd22545e105bb3b82ae17993077

--- src/gretel_client/navigator/data_designer/aidd_config.py
+++ src/gretel_client/navigator/data_designer/aidd_config.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from gretel_client.navigator.data_designer.data_column import GeneratedDataColumn
 from gretel_client.navigator.tasks.types import (
@@ -25,7 +25,7 @@ class PostProcessEvaluator(PostProcessor):
 
 class AIDDConfig(BaseModel):
     model_suite: ModelSuite
-    categorical_seed_columns: list[SeedCategory]
+    categorical_seed_columns: list[SeedCategory] = Field(..., min_length=1)
     generated_data_columns: list[GeneratedDataColumn]
     post_processors: Optional[
         list[Union[PostProcessValidator, PostProcessEvaluator]]
