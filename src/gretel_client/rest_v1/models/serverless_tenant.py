56e506d565dbfc9aa8a425db7f16a4b734e2d9b2
Taylor Mutch <taylor.mutch@gretel.ai>
2025-03-05 20:30:33 -0800

    [INTERNAL] Remove creation of cluster object when creating a serverless tenant
    GitOrigin-RevId: e27fb191b92dd7dcca10b10df856b1508d3bef56

--- src/gretel_client/rest_v1/models/serverless_tenant.py
+++ src/gretel_client/rest_v1/models/serverless_tenant.py
@@ -35,7 +35,7 @@ from gretel_client.rest_v1.models.serverless_tenant_provisioning_state import (
 
 class ServerlessTenant(BaseModel):
     """
-    ServerlessTenant
+    Next Tag: 9
     """  # noqa: E501
 
     guid: StrictStr
@@ -46,7 +46,7 @@ class ServerlessTenant(BaseModel):
     config: ServerlessTenantConfig
     cluster_guid: Optional[StrictStr] = Field(
         default=None,
-        description="The GUID of the cluster that the tenant is associated with.",
+        description="The GUID of the cluster that the tenant is associated with. Deprecated: No longer used.",
     )
     provisioning_state: Optional[ServerlessTenantProvisioningState] = None
     __properties: ClassVar[List[str]] = [
